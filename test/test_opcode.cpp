91c5fb65d205c95b5e85c8d9a41e9a252854c51b
Luis Zuniga <luis@luiszun.com>
2022-12-19 00:03:32 -0800

    Add more opcodes, fix test.

--- test/test_opcode.cpp
+++ test/test_opcode.cpp
@@ -17,7 +17,8 @@ TEST(TestOpCodes_Overlapping, BasicAssertions)
         auto opCodeStart = std::get<1>(opCode) << (nOperands * 4);
 
         const auto nextOpCode = opCodeStart + (1 << (nOperands * 4));
-        std::cout << std::hex << opName << " 0x" << std::setfill('0') << std::setw(4) << opCodeStart << " 0x" << std::setfill('0') << std::setw(4) << nextOpCode << std::endl;
+        std::cout << std::hex << opName << " 0x" << std::setfill('0') << std::setw(4) << opCodeStart << " 0x" << std::setfill('0') << std::setw(4) << nextOpCode - 1 << std::endl;
+        ASSERT_TRUE(opCodeStart < 0x10000);
         for (auto i = opCodeStart; i < nextOpCode; ++i)
         {
             ASSERT_EQ(usedOpcode[i], 0);
