1aee28d81d3323360f6f460fd75dc513501bd794
Luis Zuniga <luis@luiszun.com>
2022-12-19 21:19:16 -0800

    Fixes off-by-1 on test. Build with clang. Add clang-format. Fix gtest for clang.

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # LuinixCPU_Emu
 
 This is an emulator implemented for the Luinux CPU architecture. Defined as a mock/educational CPU, and described in the Monkey See Monkey Code texts.
+
+This project uses CMake.
+
+```
+$ mkdir build
+$ cd build
+$ cmake ..
+$ cmake --build .
+````
+
+NOTE: clang hits this problem with C++20, so we C++17 is being specified
+https://github.com/google/googletest/issues/3659
--- test/test_opcode.cpp
+++ test/test_opcode.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
 #include <algorithm>
-#include <iomanip>
 #include <gtest/gtest.h>
+#include <iomanip>
+#include <iostream>
 
 #include <opcode.h>
 
 TEST(TestOpCodes_Overlapping, BasicAssertions)
 {
-    std::array<uint8_t, 0xffff> usedOpcode;
+    std::array<uint8_t, 0x10000> usedOpcode;
     usedOpcode.fill(0);
 
     for (auto &opCode : opCodeTable)
@@ -17,12 +17,40 @@ TEST(TestOpCodes_Overlapping, BasicAssertions)
         auto opCodeStart = std::get<1>(opCode) << (nOperands * 4);
 
         const auto nextOpCode = opCodeStart + (1 << (nOperands * 4));
-        std::cout << std::hex << opName << " 0x" << std::setfill('0') << std::setw(4) << opCodeStart << " 0x" << std::setfill('0') << std::setw(4) << nextOpCode - 1 << std::endl;
+        std::cout << std::hex << opName << " 0x" << std::setfill('0') << std::setw(4) << opCodeStart << " 0x"
+                  << std::setfill('0') << std::setw(4) << nextOpCode - 1 << std::endl;
         ASSERT_TRUE(opCodeStart < 0x10000);
         for (auto i = opCodeStart; i < nextOpCode; ++i)
         {
             ASSERT_EQ(usedOpcode[i], 0);
             usedOpcode[i] = 1;
         }
+    }
+
+    // Indates when we got the beginning of the free opcodes.
+    // Now we need to find the end
+    bool gotBeginning = false;
+    for (auto i = 0; i < 0xffff; ++i)
+    {
+        if (!gotBeginning)
+        {
+            if (usedOpcode[i] != 0)
+                continue;
+            gotBeginning = true;
+            std::cout << "Free opcode from 0x" << std::hex << std::setfill('0') << std::setw(4) << i;
+        }
+        else
+        {
+            if (usedOpcode[i] == 0)
+                continue;
+            gotBeginning = false;
+            std::cout << " to 0x" << std::hex << std::setfill('0') << std::setw(4) << i - 1 << std::endl;
+        }
+    }
+
+    // Did we finish with an open range?
+    if (gotBeginning)
+    {
+        std::cout << " to 0xffff" << std::endl;
     }
 }
