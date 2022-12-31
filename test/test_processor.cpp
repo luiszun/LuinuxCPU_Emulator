77211164986a27a49e45b2f60966953786dc7aa6
Luis Zuniga <luis@luiszun.com>
2022-12-31 11:42:02 -0800

    test_processor 2nd test (incomplete)

--- test/test_processor.cpp
+++ test/test_processor.cpp
@@ -5,8 +5,13 @@
 #include "processor.h"
 
 using Memory16 = Memory<uint16_t>;
-TEST(TestOpCodesSuite, TestProcessorExistence)
+TEST(TestProcessorSuite, TestProcessorExistence)
 {
     Memory16 programMemory(0x10000);
     Processor cpu(programMemory);
+}
+
+TEST(TestProcessorSuite, TestFetchAndDecode)
+{
+    std::string loop10xShellCode = "\x25\x76\x00\x01\x26\x76\x01\x00\x67\x45\x25\x76\x0e\x00";
 }
